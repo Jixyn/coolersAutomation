c29c04ba7ac6a71a1e10296498cb657a403f9cec
Jacob Eliasson <jacob.eliasson.se@gmail.com>
2022-02-23 16:33:01 +0100

    Update test.js

--- project/test.js
+++ project/test.js
@@ -2,11 +2,16 @@ const {Browser, By, Key, until} = require("selenium-webdriver");
 const {suite} = require("selenium-webdriver/testing"); 
 const assert = require("assert"); 
 
+const url = "https://coolors.co"
+
 suite(function(env) {
     descrive('Coolors website', function(){
-        it('has Generator button', function() {
-            assert(1 === 2);
+        it('has Generator button', async function() {
+            let driver = await env.builder().build(); 
+            await driver.get(url); 
+            let elements = await driver.findElements(By.id('homepage_hero_text_btns')); 
+            assert(elements.length > 0);
+            driver.quit(); 
+                }); 
         }); 
-
-    })
-});  
+    });
